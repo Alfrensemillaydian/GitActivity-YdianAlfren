c1d301ad077aa50fd21b0230f514d9d0d8a97815
YdianAlfren <ydian2001@gmail.com>
2023-03-09 11:53:31 +0800

    My Educational Background added

--- git-python-ydian.py
+++ git-python-ydian.py
@@ -9,3 +9,9 @@ Created on Thu Mar  9 11:43:45 2023
 print("Full Name: Alfren Semilla Ydian")
 print("Address: Bahi Gasan, Marinduque")
 print("Birthday: September 14, 2001")
+
+# Displaying educational background
+print("College: Maarindue State College")
+print("Course: Bachelor of Science in Information Technology")
+print("Year: 2nd year")
+print("Section: C ")
