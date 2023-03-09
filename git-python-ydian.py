c37fd5a069ac165aca6095b30bb215901d2ca4c1
YdianAlfren <ydian2001@gmail.com>
2023-03-09 11:47:58 +0800

    My Personal Info added

---
+++ git-python-ydian.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  9 11:43:45 2023
+
+@author: User
+"""
+
+# Displaying personal information
+print("Full Name: Alfren Semilla Ydian")
+print("Address: Bahi Gasan, Marinduque")
+print("Birthday: September 14, 2001")
